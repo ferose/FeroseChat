631f29d51256ef31d006856a120aeb4b7745b2a4
ferose <ferose2@gmail.com>
2014-05-20 22:56:10 -0400

    works

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-FeroseCrypt
+FeroseChat
 ===========
---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+  , exphbs  = require('express3-handlebars')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+app.use(
+  "/static", //the URL throught which you want to access to you static content
+  express.static(__dirname + "/static") //where your static content is located in your filesystem
+);
+
+app.engine('handlebars', exphbs({}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('message', function(message) {
+    socket.broadcast.emit('message', message);
+  })
+});
+
+var port = Number(process.env.PORT || 3000);
+server.listen(port);
